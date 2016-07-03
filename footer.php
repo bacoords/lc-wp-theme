848702358b9eac684c82d5ba3dfc351692f764ec
bacoords <bacoords@gmail.com>
2016-07-03 16:29:50 -0700

    update footer

--- footer.php
+++ footer.php
@@ -21,9 +21,7 @@
                 <footer id="colophon" class="site-footer" role="contentinfo">
 
                     <div class="site-info">
-                        <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'understrap' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'understrap' ), 'WordPress' ); ?></a>
-                        <span class="sep"> | </span>
-                        <?php printf( __( 'Theme: %1$s by %2$s.', 'understrap' ), 'understrap', '<a href="http://understrap.com/" rel="designer">understrap.com</a>' ); ?>
+                        
                     </div><!-- .site-info -->
 
                 </footer><!-- #colophon -->
