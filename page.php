c288462a82e6405e4eebcafbdf53f4c6f4d3fcf0
bacoords <bacoords@gmail.com>
2016-07-03 12:01:02 -0700

    update pages

--- page.php
+++ page.php
@@ -1,50 +1,18 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Template Name: Empty Page Template
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between. Good for landingpages and other types of pages where you want to add a lot of custom markup
  *
  * @package understrap
  */
 
 get_header(); ?>
 
-<div class="wrapper" id="page-wrapper">
-    
-    <div  id="content" class="container">
+                <?php while ( have_posts() ) : the_post(); ?>
 
-        <div class="row">
-        
-    	   <div id="primary" class="<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>col-md-8<?php else : ?>col-md-12<?php endif; ?> content-area">
-           
-                 <main id="main" class="site-main" role="main">
+                    <?php get_template_part( 'loop-templates/content', 'empty' ); ?>
 
-                    <?php while ( have_posts() ) : the_post(); ?>
-
-                        <?php get_template_part( 'loop-templates/content', 'page' ); ?>
-
-                        <?php
-                            // If comments are open or we have at least one comment, load up the comment template
-                            if ( comments_open() || get_comments_number() ) :
-                                comments_template();
-                            endif;
-                        ?>
-
-                    <?php endwhile; // end of the loop. ?>
-
-                </main><!-- #main -->
-               
-    	    </div><!-- #primary -->
-            
-            <?php get_sidebar(); ?>
-
-        </div><!-- .row -->
-        
-    </div><!-- Container end -->
-    
-</div><!-- Wrapper end -->
+                <?php endwhile; // end of the loop. ?>
 
 <?php get_footer(); ?>
