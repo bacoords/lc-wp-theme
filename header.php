72420651912898e27a67afae930f7c49fdf3741f
Brian Coords <bacoords@gmail.com>
2016-12-11 11:01:21 -0800

    update navbar

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
                                 </button>
 
                                 <!-- Your site title as branding in the menu -->
-                                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+                                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="http://josh.exposureworldphotography.com/wp-content/uploads/2016/07/LCCF-Logo-01.png" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"></a>
 
                             </div>
 
